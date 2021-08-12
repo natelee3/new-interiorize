f6f7417a7cb4ef087d402d614de253d2ef2595ae
Sarah dePalo <sarahdepalo99@gmail.com>
2021-08-12 15:18:19 -0400

    added a redirect route for all routes that aren't already declared

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
 import Homepage from "./components/homepage/Homepage";
 import StyleQuiz from "./components/styleQuiz/StyleQuiz";
@@ -18,6 +18,9 @@ function App() {
           </Route>
           <Route exact path="/user-profile">
             <UserProfile />
+          </Route>
+          <Route path="*">
+            <Redirect to="/" />
           </Route>
         </Switch>
       </Router>
