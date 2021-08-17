a16045ac58ab243b8c985b98fb1658019406c5c7
Nate Lee <nleepercussion@gmail.com>
2021-08-17 15:55:01 -0400

    Integrated Auth0 into app and navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,11 +5,15 @@ import "./navbar.css";
 import shoppingCart from "./imgs/shoppingCart.png";
 import ItemDetails from "../shop/ItemDetails";
 import { Redirect } from "react-router-dom";
+import LoginButton from "../auth0/LoginButton";
+import LogoutButton from "../auth0/LogoutButton";
+import { useAuth0 } from '@auth0/auth0-react';
 
 const Navbar = () => {
   const [active, setActive] = useState(false);
-
   const [cart, setCart] = useState([]);
+
+  const { isAuthenticated } = useAuth0();
 
   return (
     <>
@@ -24,23 +28,34 @@ const Navbar = () => {
         </button>
         <div className={!!active ? "navbar-links active" : "navbar-links"}>
           <ul>
-            <li>
-              <Link to="/shop-intro">Shop</Link>
-            </li>
-            <li>
-              <Link to="/style-quiz">Style Quiz</Link>
-            </li>
-            <li>
-              <Link to="#">Register</Link>
-            </li>
-            <li className="userProfileLink">
-              {/* If user is signed in show user Profile else show Login */}
-              <Link to="/user-profile">User Profile</Link>
-            </li>
-            <li className="cartIcon">
-              <Link to="/shopping-cart"> 
-              <img className="cartIcon" src={shoppingCart} alt="Shopping Cart" style={localStorage.getItem("Added To Cart") !==null ? { backgroundColor: "red" } : null  } /></Link>
-            </li>
+            {!isAuthenticated ? (
+              <li>
+                <LoginButton />
+              </li>
+            ) : (
+              <>
+                <li>
+                  <Link to="/shop-intro">Shop</Link>
+                </li>
+                <li>
+                  <Link to="/style-quiz">Style Quiz</Link>
+                </li>
+                <li>
+                  <LogoutButton />
+                </li>
+                <li className="userProfileLink">
+                  {/* If user is signed in show user Profile else show Login */}
+                  <Link to="/user-profile">User Profile</Link>
+                </li>
+                <li className="cartIcon">
+                  <Link to="/shopping-cart"> 
+                  <img className="cartIcon" src={shoppingCart} alt="Shopping Cart" style={localStorage.getItem("Added To Cart") !==null ? { backgroundColor: "red" } : null  } /></Link>
+                </li>
+              </>
+            )}
+
+            
+            
           </ul>
         </div>
       </nav>
