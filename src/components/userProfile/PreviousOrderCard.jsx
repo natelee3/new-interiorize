ea066617123da56a2bb0171454c0286db27e4c94
Sarah dePalo <sarahdepalo99@gmail.com>
2021-08-19 16:37:54 -0400

    started working with the new api route

--- src/components/userProfile/PreviousOrderCard.jsx
+++ src/components/userProfile/PreviousOrderCard.jsx
@@ -1,13 +1,35 @@
-const PreviousOrderCard = ({img, index}) => {
+import { useState } from "react";
+import ReactCardFlip from "react-card-flip";
+
+const PreviousOrderCard = ({index, image, name, description}) => {
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  const handleClick = (event) => {
+    event.preventDefault();
+    setIsFlipped(!isFlipped);
+  }
+
     return (
-        <div className="productContainer" key={index}>
-        <img
-          src={`https://api.interiorize.design/images/${img}`}
-          alt="item on the way"
-          className="productImage"
-        />
-        <button>Leave a Review</button>
-      </div>
+      <>
+        <ReactCardFlip isFlipped={isFlipped} flipDirection="vertical">
+        <div className="productContainer" onClick={(event) => handleClick(event)} key={`${index}_front`}>
+          <img
+            src={`https://api.interiorize.design/images/${image}`}
+            alt="item on the way"
+            className="productImage"
+          />
+          <button>View More</button>
+        </div>
+        <div className="productContainer" onClick={(event) => handleClick(event)} key={`${index}_back`}>
+          <h3>{name}</h3>
+          <p>
+           {description}
+          </p>
+          <button>View Less</button>
+        </div>
+      </ReactCardFlip>
+    
+      </>
     )
 }
 
